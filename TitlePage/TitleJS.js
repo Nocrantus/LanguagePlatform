9bebd814c4a5c86b9264e22e5315d7536b2e0c35
Nocrantus <nocrantus@gmail.com>
2025-05-06 13:05:56 -0400

    removed redundant / dead code

--- TitlePage/TitleJS.js
+++ TitlePage/TitleJS.js
@@ -48,35 +48,6 @@ function placeholder() {
 	window.alert("This function has yet to be implemented / tested");
 }
 
-function openNav1() {
-	document.getElementById("SideNav1").style.width = "250px";
-	document.getElementById("main").style.marginLeft = "250px";
-}
-  
-  function closeNav1() {
-	document.getElementById("mySidenav").style.width = "0";
-	document.getElementById("main").style.marginLeft= "0";
-}
-
-function openNav2() {
-	document.getElementById("SideNav1").style.width = "250px";
-	document.getElementById("main").style.marginLeft = "250px";
-}
-  
-  function closeNav2() {
-	document.getElementById("mySidenav").style.width = "0";
-	document.getElementById("main").style.marginLeft= "0";
-}
-
-function openNav3() {
-	document.getElementById("SideNav1").style.width = "250px";
-	document.getElementById("main").style.marginLeft = "250px";
-}
-  
-  function closeNav3() {
-	document.getElementById("mySidenav").style.width = "0";
-	document.getElementById("main").style.marginLeft= "0";
-}
 function showFeaturePopup(message) {
 	const popup = document.getElementById("feature-popup");
 	const msg = document.getElementById("feature-popup-message");
