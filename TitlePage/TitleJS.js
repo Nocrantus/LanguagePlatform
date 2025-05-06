ef288d8c680efc4ae5bb57541723bac121035317
Nocrantus <nocrantus@gmail.com>
2025-05-05 22:57:30 -0400

    demo wip

--- TitlePage/TitleJS.js
+++ TitlePage/TitleJS.js
@@ -1,17 +1,35 @@
+let isLoggedIn=false;
+
 function redirectToDeckBrowser() {
-	window.location.href = "./DeckBrowser/DeckBrowser.html";
+	if (isLoggedIn){
+		window.location.href = "./DeckBrowser/DeckBrowser.html";
+	}
+	else {
+		redirectToLogin();
+	}
 }
 
 function redirectToDeckCreator() {
-	window.location.href = "./DeckCreator/DeckCreator.html";
+	if (isLoggedIn)
+	{
+		window.location.href = "./DeckCreator/DeckCreator.html";
+	}
+	else {
+		redirectToLogin();
+	}
 }
 
 function redirectToLibraryBrowser() {
-	window.location.href = "./LibraryBrowser/LibraryBrowser.html";
+	if(isLoggedIn) {
+		window.location.href = "./LibraryBrowser/LibraryBrowser.html";
+	}
+	else {
+		redirectToLogin();
+	}	
 }
 
-function redirectToLogin() {
-	window.location.href = "./login.html";
+function setLoggedIn() {
+	isLoggedIn = true;
 }
 
 function redirectToRegister() {
@@ -20,6 +38,42 @@ function redirectToRegister() {
 	document.getElementById(`register-form`).classList.remove('hidden');
 }
 
+function redirectToLogin() {
+	window.location.href='#top-anchor';
+	document.getElementById(`register-form`).classList.add('hidden');
+	document.getElementById(`login-form`).classList.remove('hidden');
+}
+
 function placeholder() {
 	window.alert("This function has yet to be implemented / tested");
+}
+
+function openNav1() {
+	document.getElementById("SideNav1").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+}
+  
+  function closeNav1() {
+	document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("main").style.marginLeft= "0";
+}
+
+function openNav2() {
+	document.getElementById("SideNav1").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+}
+  
+  function closeNav2() {
+	document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("main").style.marginLeft= "0";
+}
+
+function openNav3() {
+	document.getElementById("SideNav1").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+}
+  
+  function closeNav3() {
+	document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("main").style.marginLeft= "0";
 }
